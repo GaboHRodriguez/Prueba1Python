90f1223589f534788568bbdb8e3963949e186cd8
GaboHRodriguez <gabohr1980@hotmail.com>
2025-05-25 03:08:35 -0300

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 print("Hola, mundo desde Python en VS Code")
+
+class Transporte:
+    def __init__(self, mover, capacidad):
+        self.mover = mover
+        self.__capacidad = capacidad  # atributo privado
